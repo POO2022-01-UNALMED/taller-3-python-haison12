153c3240e1fc0613ee2ef9c1120b0654650ea0ad
haison12 <36688293+haison12@users.noreply.github.com>
2022-03-26 21:45:32 -0500

    Update marca.py

--- televisores/marca.py
+++ televisores/marca.py
@@ -1,7 +1,11 @@
 class Marca:
-    def __init__(self,nombre):
-        self.nombre = nombre
-    def set_nombre(self,nombre):
-        self.nombre = nombre
-    def get_nombre(self):
-        return self.nombre
+    def  __init__ (self,nombre):
+        self._nombre = nombre
+    
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self,nombre2):
+        self._nombre = nombre2
+
+        
