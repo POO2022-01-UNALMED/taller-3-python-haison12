f4a9c9cc90f8ed915754f0537c89e7596258711b
haison12 <36688293+haison12@users.noreply.github.com>
2022-03-26 21:45:29 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,37 +1,38 @@
+
+
+
 class Control:
-
-    def enlazar(self, Tv):
-        self._Tv = Tv
-        self._Tv._Control = self
-
+    def enlazar(self, tv):
+        self._tv = tv
+        self._tv._control = self
+    
+    
     def turnOn(self):
-        self._Tv.turnOn()
-    def turnOff(self,estado):
-        self._Tv.turnOff()
+        self._tv.turnOn()
 
+    def turnOff(self):
+        self._tv.turnOff()
 
     def canalUp(self):
-        self._Tv.canalUp()
-
+        self._tv.canalUp()
 
     def canalDown(self):
-        self._Tv.canalDown()
-
+        self._tv.canalDown()
 
     def volumenUp(self):
-        self._Tv.volumenUp()
-
+        self._tv.volumenUp()
 
     def volumenDown(self):
-        self._Tv.volumenDown()
+        self._tv.volumenDown()
+    
+    def setCanal(self, numero):
+        if self._tv._estado and numero >= 1 and numero <= 120:
+            self._tv._estado = numero
 
+    
 
-    def setCanal(self, canal):
-        if self._Tv._estado and canal >= 1 and canal <= 120:
-            self._Tv._canal = canal
-
-    def get_tv(self):
-        return self._Tv
-
-    def volumenUp(self,Tv):
-        self._Tv = Tv
+    def setTv(self, tv):
+        self._tv = tv
+    
+    def getTv(self):
+        return self._tv
