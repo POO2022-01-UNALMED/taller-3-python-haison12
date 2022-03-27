6515836c17ff9cdaea4182b320222e27bd5afb20
haison12 <36688293+haison12@users.noreply.github.com>
2022-03-26 20:07:57 -0500

    Create Marca.py

---
+++ Televisores/Marca.py
@@ -0,0 +1,11 @@
+class Marca:
+    def __init__(self,nombre):
+        self.nombre = nombre
+    def set_nombre(self,nombre):
+        self.nombre = nombre
+    def get_nombre(self):
+        return self.nombre
+
+
+
+
